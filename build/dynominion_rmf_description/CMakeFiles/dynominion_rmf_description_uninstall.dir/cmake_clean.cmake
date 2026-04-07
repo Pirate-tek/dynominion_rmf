ae42e768f5441bc6bfb646356d0a07d95eee99ac
Pirate-tek <guruprasath.a003@gmail.com>
2026-04-07 18:27:57 +0530

    decluttering

--- build/dynominion_rmf_description/CMakeFiles/dynominion_rmf_description_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dynominion_rmf_description_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dynominion_rmf_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/teleop_robot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jazzy/Desktop/dynominion_rmf/install/teleop_robot'
